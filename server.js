715a6bc9c4d5fc793c9a4d58e152d2ed56715b56
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2025-04-09 01:41:33 +0530

    feat: Add API routes to server

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const authRoutes = require('./routes/auth');
+const apiRoutes = require('./routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
