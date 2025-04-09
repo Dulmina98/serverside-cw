4eef12aca2f0038e819eca4e79a8693b127c1797
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2025-04-09 14:25:04 +0530

    feat: Add security and performance enhancements with helmet, cors, and express-rate-limit

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 dotenv.config();
 
 const app = express();
 
+app.use(helmet());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
