3cf2596b85b27ec424ee3f4bfaeb5e14c416341c
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2025-04-09 14:21:55 +0530

    feat: Configure EJS as the view engine and add admin routes

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 const authRoutes = require('./routes/auth');
 const apiRoutes = require('./routes/api');
+const adminRoutes = require('./routes/admin');
+
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
+app.use('/admin', adminRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
