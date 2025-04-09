179ffdf10e18041713ba9d4beda1a1df31a38990
Dulmina Wijegunarathne <92733749+Dulmina98@users.noreply.github.com>
2025-04-10 01:03:37 +0530

    feat: Add navigation bar component and integrate routing in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import {BrowserRouter} from "react-router-dom";
-import AdminDashboard from "./components/admin";
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Register from "./components/register";
+import Login from "./components/login";
+import Admin from "./components/admin";
+import NavigationBar from "./components/navigation-bar";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <AdminDashboard/>
-      </BrowserRouter>
-  );
+    const [token, setToken] = useState('');
+
+    return (
+        <div className={"bg-black min-vh-100 flex-row justify-content-center align-content-center"}>
+            <NavigationBar/>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Register/>}/>
+                    <Route path="/login" element={<Login setToken={setToken}/>}/>
+                    <Route path="/admin" element={<Admin token={token}/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
